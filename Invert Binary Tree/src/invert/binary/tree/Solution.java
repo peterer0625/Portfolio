bd79767907a9710bf2039773a4d82721bd6c974e
KT <peterer0625@gmail.com>
2018-01-13 19:45:27 +0800

    [Invert Binary Tree] 1. Add description. 2. Refine code.

--- Invert Binary Tree/src/invert/binary/tree/Solution.java
+++ Invert Binary Tree/src/invert/binary/tree/Solution.java
@@ -8,12 +8,25 @@ package invert.binary.tree;
 /**
  *
  * @author KT
+ * Description
+ * Invert a binary tree.
+ *      4
+ *    /   \
+ *   2     7
+ *  / \   / \
+ * 1   3 6   9
+ * to
+ *      4
+ *    /   \
+ *   7     2
+ *  / \   / \
+ * 9   6 3   1
  */
 public class Solution
 {
     public TreeNode invertTree(TreeNode root)
     {
-        if (root.left != null && root.right != null)
+        if (root != null)
         {
             TreeNode node = root.left;
             root.left = root.right;
