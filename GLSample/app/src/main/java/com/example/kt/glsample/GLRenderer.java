9b370d7eb94acc0e31a0342d90ab2263e02956d7
KT <peterer0625@gmail.com>
2018-01-24 21:08:21 +0800

    [Android][GLSample] Add empty GLRenderer.

---
+++ GLSample/app/src/main/java/com/example/kt/glsample/GLRenderer.java
@@ -0,0 +1,26 @@
+package com.example.kt.glsample;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by KT on 2018/1/24.
+ */
+
+public class GLRenderer implements android.opengl.GLSurfaceView.Renderer
+{
+    @Override
+    public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl10, int i, int i1) {
+
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl10) {
+
+    }
+}
--- GLSample/app/src/main/java/com/example/kt/glsample/MainActivity.java
+++ GLSample/app/src/main/java/com/example/kt/glsample/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.kt.glsample;
 
+import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GLSurfaceView glSurfaceView = new GLSurfaceView(this);
+        glSurfaceView.setRenderer(new GLRenderer());
+        setContentView(glSurfaceView);
     }
 }
