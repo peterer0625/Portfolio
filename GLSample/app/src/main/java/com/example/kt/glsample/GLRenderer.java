ed6537eec9dc933de7e2e9b07f01e9133e208eec
KT <peterer0625@gmail.com>
2018-01-24 21:11:45 +0800

    [Android][GLSample] 1. Set viewport. 2. Set clear color 3. Clear

--- GLSample/app/src/main/java/com/example/kt/glsample/GLRenderer.java
+++ GLSample/app/src/main/java/com/example/kt/glsample/GLRenderer.java
@@ -15,12 +15,13 @@ public class GLRenderer implements android.opengl.GLSurfaceView.Renderer
     }
 
     @Override
-    public void onSurfaceChanged(GL10 gl10, int i, int i1) {
-
+    public void onSurfaceChanged(GL10 gl10, int width, int height) {
+        gl10.glViewport(0, 0, width, height);
     }
 
     @Override
     public void onDrawFrame(GL10 gl10) {
-
+        gl10.glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+        gl10.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
     }
 }
