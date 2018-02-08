6e5ba92e71298e329d84aa9d4af65d12d26f4f8c
KT <peterer0625@gmail.com>
2018-02-08 17:07:58 +0800

    [Android][GLSample] Refine code.

--- GLSample/app/src/main/java/com/example/kt/glsample/GLRenderer.java
+++ GLSample/app/src/main/java/com/example/kt/glsample/GLRenderer.java
@@ -1,5 +1,7 @@
 package com.example.kt.glsample;
 
+import android.opengl.GLES20;
+
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
@@ -9,6 +11,7 @@ import javax.microedition.khronos.opengles.GL10;
 
 public class GLRenderer implements android.opengl.GLSurfaceView.Renderer
 {
+
     @Override
     public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
 
@@ -16,12 +19,12 @@ public class GLRenderer implements android.opengl.GLSurfaceView.Renderer
 
     @Override
     public void onSurfaceChanged(GL10 gl10, int width, int height) {
-        gl10.glViewport(0, 0, width, height);
+        GLES20.glViewport(0, 0, width, height);
     }
 
     @Override
     public void onDrawFrame(GL10 gl10) {
-        gl10.glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
-        gl10.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+        GLES20.glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+        GLES20.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
     }
 }
