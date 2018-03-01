f28ff106096f59c5c71f80d3ab03ccad00bd02ad
KT <peterer0625@gmail.com>
2018-03-01 15:20:50 +0800

    [Python][Simple Tesorflow Sample 3] Refine code style.

--- Python/SimpleTesorflowSample3.py
+++ Python/SimpleTesorflowSample3.py
@@ -17,23 +17,27 @@ x = tf.placeholder(tf.float32, [None, 784])
 
 w1 = tf.Variable(tf.random_normal([784, 200]))
 b1 = tf.Variable(tf.zeros([200]))
+z1 = tf.matmul(x, w1) + b1
+a1 = tf.nn.relu(z1)
 
 w2 = tf.Variable(tf.zeros([200, 10]))
 b2 = tf.Variable(tf.zeros([10]))
+z2 = tf.matmul(a1, w2) + b2
+a2 = tf.nn.softmax(z2)
 
-y = tf.nn.softmax(tf.matmul(tf.matmul(x, w1) + b1, w2) + b2)
+y = a2
 
-y_input = tf.placeholder(tf.float32, [None, 10])
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_input * tf.log(y), reduction_indices=[1]))
+y_hat = tf.placeholder(tf.float32, [None, 10])
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_hat * tf.log(y), reduction_indices=[1]))
 train_step = tf.train.GradientDescentOptimizer(0.005).minimize(cross_entropy)
 
 tf.initialize_all_variables().run()
-for i in range(1200):
+for i in range(200):
     batch_x, batch_y = mnist.train.next_batch(100)
-    train_step.run({x: batch_x, y_input: batch_y})  
-    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_input, 1))
+    train_step.run({x: batch_x, y_hat: batch_y})  
+    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_hat, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    print(accuracy.eval({x: mnist.test.images, y_input: mnist.test.labels}))
+    print(accuracy.eval({x: mnist.test.images, y_hat: mnist.test.labels}))
     #print(w.eval())
     #print(y)
     #print(y.eval({x: mnist.test.images}))
