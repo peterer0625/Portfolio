eb5d93f4a6c06c0e69dc51d26a8aeff44e8fe4ab
KT <peterer0625@gmail.com>
2019-03-18 21:56:00 +0800

    [Android][GLSample] Add color to vertex data for Cube.

--- Android/GLSample/app/src/main/java/com/example/kt/glsample/Cube.java
+++ Android/GLSample/app/src/main/java/com/example/kt/glsample/Cube.java
@@ -1,6 +1,5 @@
 package com.example.kt.glsample;
 
-import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.opengl.GLES20;
@@ -19,8 +18,9 @@ public class Cube
 {
     private int[] mPositionHandle = new int[1];
     private int[] mIndexHandle = new int[1];
-    private int[] mTextureHandle = new int[1];
+    private int[] mColorHandle = new int[1];
 
+    private int[] mTextureHandle = new int[1];
 
     //       +y     -z
     //       |     /
@@ -37,6 +37,11 @@ public class Cube
              1.0f,  1.0f,  0.0f, 1.0f,
              1.0f, -1.0f,  0.0f, 1.0f};
 
+    private float mColorArray[] = {
+            1.0f, 0.0f, 0.0f, 1.0f,
+            1.0f, 1.0f, 1.0f, 1.0f,
+            0.0f, 0.0f, 1.0f, 1.0f};
+
     private short mIndexArray[] = { 0, 1, 2,
                                     1, 2, 3};
 
@@ -46,24 +51,30 @@ public class Cube
     private int[] mProgram = new int[1];
     private int[] mProgramPositionHandle = new int[1];
     private int[] mProgramColorHandle = new int[1];
+    private int[] mFragmentProgramColorHandle = new int[1];
     private float[] mColor = new float[4];
 
     private String mVsCode =
             "attribute vec4 vPosition;\n" +
+            "attribute vec4 vColor;\n" +
+            "varying vec4 color;\n" +
             "void main(){\n" +
+            "    color = vColor;\n" +
             "    gl_Position = vPosition;\n" +
             "}";
 
-    private String mPSCode =
+    private String mPsCode =
             "precision mediump float;\n" +
             "uniform vec4 uColor;\n" +
+            "varying vec4 color;\n" +
             "void main(){\n" +
-            "    gl_FragColor = uColor;\n" +
+            "    gl_FragColor = uColor + color;\n" +
             "}";
 
     public Cube()
     {
         this.initPosition();
+        this.initColorData();
         this.initIndex();
         this.initShader();
 
@@ -101,6 +112,22 @@ public class Cube
         GLES20.glBindBuffer(GLES20.GL_ELEMENT_ARRAY_BUFFER, 0);
     }
 
+    private void initColorData()
+    {
+        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(mColorArray.length * Cube.sBytesPerFloat);
+        byteBuffer.order(ByteOrder.nativeOrder());
+        FloatBuffer floatBuffer = byteBuffer.asFloatBuffer();
+        floatBuffer = byteBuffer.asFloatBuffer();
+        floatBuffer.put(mColorArray);
+        floatBuffer.position(0);
+
+        GLES20.glGenBuffers(1, mColorHandle, 0);
+        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, mColorHandle[0]);
+        GLES20.glBufferData(GLES20.GL_ARRAY_BUFFER, floatBuffer.capacity() * Cube.sBytesPerFloat, floatBuffer, GLES20.GL_STATIC_DRAW);
+
+        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, 0);
+    }
+
     private void initShader()
     {
         int vertexShader = GLES20.glCreateShader(GLES20.GL_VERTEX_SHADER);
@@ -108,7 +135,7 @@ public class Cube
         GLES20.glCompileShader(vertexShader);
 
         int pixelShader = GLES20.glCreateShader(GLES20.GL_FRAGMENT_SHADER);
-        GLES20.glShaderSource(pixelShader, this.mPSCode);
+        GLES20.glShaderSource(pixelShader, this.mPsCode);
         GLES20.glCompileShader(pixelShader);
 
         this.mProgram[0] = GLES20.glCreateProgram();
@@ -118,7 +145,9 @@ public class Cube
 
         GLES20.glUseProgram(this.mProgram[0]);
         this.mProgramPositionHandle[0] = GLES20.glGetAttribLocation(this.mProgram[0],"vPosition");
-        this.mProgramColorHandle[0] = GLES20.glGetUniformLocation(this.mProgram[0],"uColor");
+        this.mProgramColorHandle[0] = GLES20.glGetAttribLocation(this.mProgram[0],"vColor");
+
+        this.mFragmentProgramColorHandle[0] = GLES20.glGetUniformLocation(this.mProgram[0],"uColor");
     }
 
     private void initTexture()
@@ -138,20 +167,29 @@ public class Cube
 
     public void draw()
     {
-        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, this.mPositionHandle[0]);
+        GLES20.glUseProgram(this.mProgram[0]);
+        GLES20.glEnableVertexAttribArray(this.mProgramPositionHandle[0]);
+        GLES20.glEnableVertexAttribArray(this.mProgramColorHandle[0]);
+
         GLES20.glBindBuffer(GLES20.GL_ELEMENT_ARRAY_BUFFER, this.mIndexHandle[0]);
         GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, this.mTextureHandle[0]);
 
-        GLES20.glUseProgram(this.mProgram[0]);
-        GLES20.glEnableVertexAttribArray(this.mProgramPositionHandle[0]);
+        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, this.mPositionHandle[0]);
         GLES20.glVertexAttribPointer(this.mProgramPositionHandle[0], 4, GLES20.GL_FLOAT, false, 0, 0);
 
-        GLES20.glUniform4fv(this.mProgramColorHandle[0],1, this.mColor, 0);
+        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, this.mColorHandle[0]);
+        GLES20.glVertexAttribPointer(this.mProgramColorHandle[0], 4, GLES20.GL_FLOAT, false, 0, 0);
+
+        GLES20.glUniform4fv(this.mFragmentProgramColorHandle[0],1, this.mColor, 0);
 
         GLES20.glDrawElements(GLES20.GL_TRIANGLES, mIndexArray.length, GLES20.GL_UNSIGNED_SHORT, 0);
 
         GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, 0);
         GLES20.glBindBuffer(GLES20.GL_ELEMENT_ARRAY_BUFFER, 0);
+
+        GLES20.glDisableVertexAttribArray(this.mProgramPositionHandle[0]);
+        GLES20.glDisableVertexAttribArray(this.mProgramColorHandle[0]);
+        GLES20.glUseProgram(0);
     }
 
     private void setColors(float r, float g, float b)
